83603ce485d402e594b0c254a2e8c1e3b15cc5cc
Donovan Green <donovangreen360@gmail.com>
2022-07-31 21:49:35 -0400

    Created template for half function

---
+++ C++ Week 11 - Part B/Source.cpp
@@ -0,0 +1,29 @@
+// Donovan Green
+// CIS 1202
+// 7-31-2022
+// Template test
+
+#include<iostream>
+
+using namespace std;
+
+// *******************
+//      TEMPLATES
+// *******************
+
+template <typename T>
+T half(T number) {
+	return T / 2;
+}
+
+// ***********************
+//      MAIN FUNCTION
+// ***********************
+
+int main() {
+
+	cout << "Thank you for using my program!";
+	cout << "\n\n";
+	system("pause");
+	return 0;
+}
